4fb4f8de871a028d6530194822f48fcb4b8e5491
Ikecruz <ikedinobicruz7@gmail.com>
2021-12-05 19:38:17 +0100

    Started learning react

---
+++ src/Cruz.js
@@ -0,0 +1,14 @@
+import './Cruz.css'
+
+const Cruz = () => {
+
+    let title = "My blog";
+
+    return(
+        <div className="App">
+            <div>{ title }</div>
+        </div>
+    );
+}
+
+export default Cruz;
