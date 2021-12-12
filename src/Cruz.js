fae68706f84ea3b11f73c02f93e6e6082219bb47
Ikecruz <ikedinobicruz7@gmail.com>
2021-12-12 19:50:12 +0100

    Displaying list

--- src/Cruz.js
+++ src/Cruz.js
@@ -1,12 +1,16 @@
-import './Cruz.css'
+import './Cruz.css';
+
+import { useState } from 'react';
 
 const Cruz = () => {
 
-    let title = "My blog";
+    let [nums, setNums] = useState([1, 2, 3, 4, 5, 5, 6, ]);
 
     return(
         <div className="App">
-            <div>{ title }</div>
+            <div>{ nums.map((num, index) => (
+                <div key={index}>{num}</div>
+            )) }</div>
         </div>
     );
 }
